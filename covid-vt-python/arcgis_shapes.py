506a2c5a74e7cd663456a2b93cb4ccae8b2044d3
Daniel Hyun <dkhyun64@outlook.com>
2022-01-27 21:09:32 -0500

    table name update

--- covid-vt-python/arcgis_shapes.py
+++ covid-vt-python/arcgis_shapes.py
@@ -1,6 +1,6 @@
 import requests
 import geopandas as gpd
-import psycopg
+import psycopg2
 import json
 
 url = "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties/FeatureServer/0/query?where=STATE_NAME%20%3D%20'VERMONT'&outFields=*&outSR=4326&f=geojson"
@@ -11,12 +11,12 @@ data = response.json()
 
 # gdf = gpd.GeoDataFrame.from_features(data['features'])
 
-conn = psycopg.connect(host='localhost', port=5432, dbname='covid_vt', user='postgres', password='djneosan')
+conn = psycopg2.connect(host='localhost', port=5432, dbname='covid_vt', user='postgres', password='pearlriver')
 
 with conn.cursor() as cur:
-    cur.execute('truncate table raw_geojson')
+    cur.execute('truncate table raw_geojsonb')
     for i in data['features']:
-        cur.execute('insert into raw_geojson (feature_collection) values (%s)', (json.dumps(i),))
+        cur.execute('insert into raw_geojsonb (feature_collection) values (%s)', (json.dumps(i),))
     print('loading data complete')
 
 conn.commit()
